14e0ab9c6ffc1f4385c1df9c7e279e9172393fc0
Stephanie Fitzgerald <s.e.v.fitzgerald@gmail.com>
2017-05-09 18:11:45 -0700

    Move calcCookiesPerHour

--- js/app.js
+++ js/app.js
@@ -19,10 +19,9 @@ function Store ( id, name, minCust, maxCust, avgCookiesPerCust, cookieNeed ) {
 
     // Array filled from calcCookiesPerHr
     this.cookieNeed = cookieNeed;
-
-    // One method for this object: calculate the cookies per hour (for each store)
-    this.calcCookiesPerHr = calcCookiesPerHr;
 }
+
+var hours = [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
 
 var store1 = new Store( 1, 'PDX Airport', 23, 65, 6.3, [] );
 var store2 = new Store( 2, 'Pioneer Square', 3, 24, 1.2, [] );
@@ -31,16 +30,29 @@ var store4 = new Store( 4, 'St. John\'s', 20, 38, 2.3, [] );
 var store5 = new Store( 5, 'Waterfront', 2, 16, 4.6, [] );
 
 Store.prototype.storeHours = '6:00 AM - 8:00 PM';
+Store.prototype.calcCookiesPerHr = function () {
+    for ( var h = 0; h < hours.length; h++ ) {
+        var randCust = Math.floor(Math.random() * ( this.maxCust - this.minCust ) + this.minCust +1 );
+        var cookies = Math.round(randCust * this.avgCookiesPerCust);
+        this.cookieNeed.push( cookies );
+    }
+};
+
+store1.calcCookiesPerHr();
+store2.calcCookiesPerHr();
+store3.calcCookiesPerHr();
+store4.calcCookiesPerHr();
+store5.calcCookiesPerHr();
 
 // Hours and Stores arrays to use later
-var hours = [ '6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-var storesArray = [store1.name, store2.name, store3.name, store4.name, store5.name];
+
+var storesArray = [store1, store2, store3, store4, store5];
 
 // Helper function to create cells by row
 function render ( cellType, content, rowToAddChildTo ) {
     var cell = document.createElement( cellType );
     cell.innerText = content;
-    rowToAddChildTo.appendChild( cell );
+	rowToAddChildTo.appendChild( cell );
 }
 
 // Create the Table Head
@@ -48,7 +60,7 @@ function createTableHead () {
     var header = document.getElementById('header-row');
     render( 'th', 'Hours', header );
     for ( var j = 0; j < storesArray.length; j++ ) {
-        render( 'th', storesArray[j], header );
+        render( 'th', storesArray[j].name, header );
     }
 }
 createTableHead();
@@ -59,35 +71,38 @@ createTableHead();
 //   //  var loc = storesArray[i];
 //   //  total += loc.calcCookiesPerHr();
 //   total += store1.cookieNeed[i];
-  
-  
 //     console.log(total);
 
 // }
 
 
-// // Create the Table Footer
-// function createTableFooter () {
-//     var footer = document.getElementById('footer-row');
 
-//     for ( var j = 0; j < cookies.length; j++ ) {
-//         render( 'th', cookies[j], footer );
-//     }
-// }
-// createTableFooter();
 
 // Create the Table Data
 function createTableData () {
     var body = document.getElementById('table-body');
-    // each row needs a th with the hour
-    // each row needs one td for each store with this.cookieNeed[i]
 
-    for ( var j = 0; j < hours.length; j++ ) {
-        render( 'tr', 'test', body );
+    for ( var k = 0; k < hours.length; k++ ) {
+        var tr = document.createElement('tr');
+        body.appendChild(tr);
+        render( 'td', hours[k], tr );
 
+        for ( var l = 0; l < storesArray.length; l++ ) {
+            render('td', storesArray[l].cookieNeed[k], tr );
+        }
     }
 }
 createTableData();
+    
+// // Create the Table Footer
+// function createTableFooter () {
+//     var footer = document.getElementById('footer-row');
+
+//     for ( var j = 0; j < totalsArray.length; j++ ) {
+//         render( 'tr', totalsArray[j], footer );
+//     }
+// }
+// createTableFooter();
 
 
 // for ( var j = 0; j < storesArray.length; j++ ) {
@@ -104,9 +119,9 @@ createTableData();
 
 
 
-// for ( var j = 0; j < stores.length; j++ ) {
+// for ( var j = 0; j < storesArray.length; j++ ) {
 
-//     var store = stores[j];
+//     var store = storesArray[j];
 //     var storeSection = document.getElementById('store-' + store.id);
     
 //     var ellh = document.createElement('lh');
